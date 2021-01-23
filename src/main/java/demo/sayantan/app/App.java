2691480029cfada7e9c43e83f0ea62fc7e8c3c5e
sayantan.sb <sayantan.bhattacharya@idealo.de>
2021-01-24 00:10:53 +0100

    Initial Commit

---
+++ src/main/java/demo/sayantan/app/App.java
@@ -0,0 +1,13 @@
+package demo.sayantan.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new SpringApplicationBuilder(App.class).run(args);
+    }
+}
