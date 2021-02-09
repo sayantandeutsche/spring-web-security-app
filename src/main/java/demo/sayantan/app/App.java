e7b5aad4a15027b7d468ffc3eee72fdd10835fc0
sayantan.sb <sayantan.bhattacharya@idealo.de>
2021-02-09 14:15:39 +0100

    Added buildspec

--- src/main/java/demo/sayantan/app/App.java
+++ src/main/java/demo/sayantan/app/App.java
@@ -2,9 +2,10 @@ package demo.sayantan.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App 
+public class App extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
